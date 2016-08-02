f18e2da7fca13cc4e2272acc27104fb4c0f36c9d
zkxok <zkx.ok@qq.com>
2016-08-02 23:03:18 +0800

    第四次提交，自定义SeekBar,完成了视频界面布局,完成播放/暂停状态切换

--- app/src/main/java/zkx/com/mobileplayer/ui/activity/VideoPlayerActivity.java
+++ app/src/main/java/zkx/com/mobileplayer/ui/activity/VideoPlayerActivity.java
@@ -1,7 +1,9 @@
 package zkx.com.mobileplayer.ui.activity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.VideoView;
 
@@ -13,6 +15,7 @@ import zkx.com.mobileplayer.bean.VideoItem;
  */
 public class VideoPlayerActivity extends BaseActivity {
     private VideoView videoView;
+    private ImageView iv_pause;
 
     @Override
     public int getLayoutId() {
@@ -22,11 +25,13 @@ public class VideoPlayerActivity extends BaseActivity {
     @Override
     public void initView() {
         videoView = (VideoView) findViewById(R.id.videoplayer_videoview);
+        iv_pause = (ImageView) findViewById(R.id.videoplayer_bottom_iv_pause);
     }
 
     @Override
     public void initListener() {
-
+        iv_pause.setOnClickListener(this);
+        videoView.setOnPreparedListener(new OnVideoPreparedListener());
     }
 
     @Override
@@ -37,12 +42,53 @@ public class VideoPlayerActivity extends BaseActivity {
 
         videoView.setVideoURI(Uri.parse(videoItem.getPath()));
 //        videoView.setVideoPath(videoItem.getPath());
-        videoView.start();
-        videoView.setMediaController(new MediaController(this));
+//        videoView.start();
+//        videoView.setMediaController(new MediaController(this));
     }
 
     @Override
     public void processClick(View v) {
+        switch (v.getId()){
+            case R.id.videoplayer_bottom_iv_pause:
+                switchPauseStatus();
+                break;
+        }
+    }
 
+    /**
+     * 切换视频播放/暂停状态
+     */
+    private void switchPauseStatus() {
+        if(videoView.isPlaying()){
+            //正在播放,点击按钮变暂停
+            videoView.pause();
+        }else{
+            //暂停状态,点击按钮变播放
+            videoView.start();
+        }
+        updatePauseBtn();
+    }
+
+    /**
+     * 根据当前播放状态，更改暂停按钮使用的图片
+     */
+    private void updatePauseBtn() {
+        if (videoView.isPlaying()){
+            //正在播放
+            iv_pause.setImageResource(R.drawable.video_pause_seletor);
+        }else{
+            //暂停
+            iv_pause.setImageResource(R.drawable.video_paly_seletor);
+        }
+    }
+
+    private class OnVideoPreparedListener implements MediaPlayer.OnPreparedListener {
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            //视频准备完成
+            videoView.start();
+            //更新暂停按钮使用的图片
+            updatePauseBtn();
+        }
     }
 }
